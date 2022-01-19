d7debfbcd0e0fe3ddc1a5613bf7c1f4dd21e593b
Mia Hunt <miaxyhunt@gmail.com>
2022-01-19 09:44:39 -0800

    Delete lf

--- install.sh
+++ install.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-ln -s $(pwd)/.* "$HOME/"
-# ln -s ./.config/* "$HOME/.config/"
-# ln -s ./.config/* $HOME/.config/
-# for file in $(pwd)/.*
-# do
-#     ln -s "$file" "$HOME/$file" echo "Linked $file"
-# done
-# 
-# for dir in ./.config/
-# do
-#     ln -s "$dir" "$HOME/.config/$dir" echo "Linked $dir"
-# done
+if [[ -n "$@" ]]; then
+    files="$@"
+
+    # Redefine myvar to files using parenthesis
+    file_arr=($files)
+
+    # echo "My array: ${file_arr[@]}"
+    # echo "Number of elements in the array: ${#file_arr[@]}"
+    for file in "${file_arr[@]}"
+    do 
+        if [[ $file = .* ]]; then 
+            ln -s "$(pwd)/$file" "$HOME/$file" && echo "Linked $(pwd)/$file -> $HOME/$file"
+        else
+            ln -s "$(pwd)/.config/$file" "$HOME/.config/" && echo "Linked $(pwd)/.config/$file -> $HOME/.config/$file"
+        fi
+    done
+else
+    for file in $(pwd)/.*
+    do 
+        ln -s "$file" "$HOME/" && echo "Linked $file -> $HOME/$file"
+    done
+    for file in $(pwd)/.config/*
+    do 
+        ln -s "$file" "$HOME/.config/" && echo "Linked $file -> $HOME/.config/$file"
+    done
+fi
--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-for file in $HOME/.*
-do
-   ## echo "$file"
-    [[ -L "$file" ]] && unlink "$file" && echo "Unlinked $file"
-done
+if [[ -n "$@" ]]; then
+    files="$@"
 
-for dir in $HOME/.config/
-do
-   ## echo "$dir"
-    [[ -L "$dir" ]] && unlink "$dir" && echo "Unlinked $dir"
-done
+    # Redefine myvar to files using parenthesis
+    file_arr=($files)
+
+    # echo "My array: ${file_arr[@]}"
+    # echo "Number of elements in the array: ${#file_arr[@]}"
+    for file in "${file_arr[@]}"
+    do 
+        [[ $file = .* ]] && file="$HOME/$file" || file="$HOME/.config/$file"
+        if [[ -L "$file" ]]; then
+            unlink "$file" && echo "Unlinked $file"
+        fi
+    done
+else
+    for file in $HOME/.*
+    do
+       ## echo "$file"
+        [[ -L "$file" ]] && unlink "$file" && echo "Unlinked $file"
+    done
+
+    for dir in $HOME/.config/*
+    do
+       ## echo "$dir"
+        [[ -L "$dir" ]] && unlink "$dir" && echo "Unlinked $dir"
+    done
+fi
