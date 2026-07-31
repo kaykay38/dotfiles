4c98642acb036489e38a452a5dab1c19fddbb692
Mia Hunt <miaxyhunt@gmail.com>
2026-07-31 15:29:10 -0600

    chore(dotfiles): migrate terminals + update tmux, yazi, shell configs
    - alacritty: migrate config from YAML to TOML; bump themes submodule
    - ghostty: add config (terminal emulator migration)
    - tmux: rework configuration
    - yazi: add yazi.toml, init.lua, and epub plugin
    - add root .luarc.json (lua-language-server settings)
    - .zshenv tweaks; remove stray scratch file

---
+++ .config/yazi/init.lua
@@ -0,0 +1,13 @@
+-- ~/.config/yazi/init.lua
+function Linemode:custom_mtime()
+    local time = math.floor(self._file.cha.mtime or 0)
+    if time == 0 then
+        time = ""
+    elseif os.date("%Y", time) == os.date("%Y") then
+        time = os.date("%b %d %H:%M", time)
+    else
+        time = os.date("%b %d  %Y", time)
+    end
+
+    return string.format("%s", time)
+end
--- .zshenv
+++ .zshenv
@@ -1,6 +1,6 @@
 export BROWSER="firefox"
 export TERMINAL="alacritty"
-export EDITOR="lvim"
-export VISUAL="code"
+export EDITOR="nvim"
+export VISUAL="codium"
 export _JAVA_AWT_WM_NONREPARENTING=1
 export PATH=$PATH:$HOME/dotnet:$HOME/.dotnet/tools:$HOME/node_modules/.bin
